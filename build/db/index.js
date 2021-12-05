4fd07d4cdd5f6555c3ddbbb5b41a378c81b9b74f
webersilv4 <webercost4@hotmail.com>
2021-12-05 12:09:38 -0300

    Update

---
+++ build/db/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var mongoose_1 = __importDefault(require("mongoose"));
+var connection = mongoose_1.default.connect(process.env.MONGODB_URL_PRODUCTION);
+exports.default = connection;
